6284210d9a1fb7664eadf8480139e957d167a206
Samuel Oreste Abreu - s281568 <orestsam@protonmail.com>
2021-11-03 12:50:12 +0100

    Feat: added frequency-based heuristics

--- tsp.py
+++ tsp.py
@@ -10,7 +10,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from random import shuffle
 
-NUM_CITIES = 23
+NUM_CITIES = 42
 STEADY_STATE = 1000
 
 
@@ -59,10 +59,31 @@ class Tsp:
         return self._graph
 
 
-def swap(new_solution):
+def get_indexes(max_int, counts):
 
-    i1 = np.random.randint(0, new_solution.shape[0])
-    i2 = np.random.randint(0, new_solution.shape[0])
+    # Select indexes that were the least used
+
+    i1, i2 = np.random.choice(range(NUM_CITIES), 2, p=counts/sum(counts))
+
+    while i1 == i2:
+        i2 = np.random.choice(range(NUM_CITIES), 1, p=counts/sum(counts))[0]
+
+    counts[0:i1] += 1
+    counts[i1+1::] += 1
+    counts[0:i2] += 1
+    counts[i2+1::] += 1
+
+    if i1 > i2:
+        tmp = i1
+        i1 = i2
+        i2 = tmp
+
+    return i1, i2
+
+
+def swap(new_solution, indexes):
+
+    i1, i2 = indexes[0], indexes[1]
     temp = new_solution[i1]
     new_solution[i1] = new_solution[i2]
     new_solution[i2] = temp
@@ -70,26 +91,18 @@ def swap(new_solution):
     return new_solution
 
 
-def scramble(new_solution):
+def scramble(new_solution, counts):
 
     for i in range(np.random.randint(new_solution.shape[0])):
-        new_solution = swap(new_solution)
+        new_solution = swap(new_solution, get_indexes(
+            new_solution.shape[0], counts))
 
     return new_solution
 
 
-def insert(new_solution):
+def insert(new_solution, indexes):
 
-    i1 = np.random.randint(0, new_solution.shape[0])
-    i2 = np.random.randint(0, new_solution.shape[0])
-
-    while i1 == i2:
-        i2 = np.random.randint(0, new_solution.shape[0])
-
-    if i1 > i2:
-        tmp = i1
-        i1 = i2
-        i2 = tmp
+    i1, i2 = indexes[0], indexes[1]
 
     tmp = list(new_solution[i1+1:i2])
     new_solution[i1+1] = new_solution[i2]
@@ -99,47 +112,50 @@ def insert(new_solution):
     return new_solution
 
 
-def inversion(new_solution):
+def inversion(new_solution, indexes):
 
-    i1 = np.random.randint(0, new_solution.shape[0])
-    i2 = np.random.randint(0, new_solution.shape[0])
-
-    while i1 == i2:
-        i2 = np.random.randint(0, new_solution.shape[0])
-
-    if i1 > i2:
-        tmp = i1
-        i1 = i2
-        i2 = tmp
-
+    i1, i2 = indexes[0], indexes[1]
     new_solution[i1:i2] = new_solution[i1:i2][::-1]
 
     return new_solution
 
 
-def tweak(solution: np.array, *, pm: float = .1) -> np.array:
+def tweak(solution: np.array, counts, *, pm: float = .1) -> np.array:
     new_solution = solution.copy()
     p = None
+
     while p is None or p < pm:
-        rand = np.random.randint(0, 4)
+
+        # Randomly select a mutation strategy
+
+        rand = np.random.randint(0, NUM_ALGO)
 
         if rand == 0:
-            new_solution = scramble(new_solution)
+            new_solution = swap(
+                new_solution, get_indexes(new_solution.shape[0], counts))
         elif rand == 1:
-            new_solution = swap(new_solution)
+            new_solution = scramble(
+                new_solution, counts)
         elif rand == 2:
-            new_solution = inversion(new_solution)
+            new_solution = insert(
+                new_solution, get_indexes(new_solution.shape[0], counts))
         else:
-            new_solution = insert(new_solution)
+            new_solution = inversion(
+                new_solution, get_indexes(new_solution.shape[0], counts))
 
         p = np.random.random()
 
-    return new_solution
+    return new_solution, counts
+
+
+NUM_ALGO = 4
 
 
 def main():
 
     problem = Tsp(NUM_CITIES)
+
+    counts = np.ones(NUM_CITIES)
 
     solution = np.array(range(NUM_CITIES))
     np.random.shuffle(solution)
@@ -152,7 +168,7 @@ def main():
     while steady_state < STEADY_STATE:
         step += 1
         steady_state += 1
-        new_solution = tweak(solution, pm=.5)
+        new_solution, counts = tweak(solution, counts, pm=.5)
         new_solution_cost = problem.evaluate_solution(new_solution)
         if new_solution_cost < solution_cost:
             solution = new_solution
